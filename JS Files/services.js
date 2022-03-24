e11c2fb2a8317327d7ccdb7a9c64b8e23ec6ea9c
NinaWendy <wendy.busolo@student.moringaschool.com>
2022-03-24 18:27:58 +0300

    optimize styling for all pages

--- JS Files/services.js
+++ JS Files/services.js
@@ -1,101 +1,100 @@
-$(document).ready(function() {
-  $("#p1").click(function(){
-    $("#p1").css("border-bottom", "3px solid black")
-    $("#p2").css("border-bottom", "none")
-    $("#p3").css("border-bottom", "none")
-    $("#col3").show();
-    $("#col4").show();
-    $("#col5").show();
-    $("#fluid2").css("height","200px")
-    $("#row2").css("height","200px")
-    $("#row2").show();
-    $("#row3").hide();
-    $("#row4").hide();
-  })
- 
-  $("#p2").click(function(){
-  $("#row2").toggle();
-  $("#row3").toggle();
-  $("#p2").css("border-bottom", "3px solid black")
-  $("#p3").css("border-bottom", "none")
-  $("#p1").css("border-bottom", "none")
-  $("#fluid2").css("height","410px")
-  $("#row3").css("height","410px")
-  $("#row2").hide();
-  $("#row3").show();
-  $("#row4").hide();
-  })
+$(document).ready(function () {
+    $("#p1").click(function () {
+        $("#p1").css("border-bottom", "3px solid black")
+        $("#p2").css("border-bottom", "none")
+        $("#p3").css("border-bottom", "none")
+        $("#col3").show();
+        $("#col4").show();
+        $("#col5").show();
+        $("#fluid2").css("height", "200px")
+        $("#row2").css("height", "200px")
+        $("#row2").show();
+        $("#row3").hide();
+        $("#row4").hide();
+    })
 
-  $("#p3").click(function(){
-    $("#p3").css("border-bottom", "3px solid black")
-    $("#p2").css("border-bottom", "none")
-    $("#p1").css("border-bottom", "none")
-    $("#fluid2").css("height","300px")
-    $("#row2").css("height","300px")
-    $("#row2").hide();
-    $("#row3").hide();
-    $("#row4").show();
-  }) 
+    $("#p2").click(function () {
+        $("#row2").toggle();
+        $("#row3").toggle();
+        $("#p2").css("border-bottom", "3px solid black")
+        $("#p3").css("border-bottom", "none")
+        $("#p1").css("border-bottom", "none")
+        $("#fluid2").css("height", "410px")
+        $("#row3").css("height", "410px")
+        $("#row2").hide();
+        $("#row3").show();
+        $("#row4").hide();
+    })
 
-  $("#p6").click(function(){
-    $("#row6").toggle();
-    $("#row5").toggle();
+    $("#p3").click(function () {
+        $("#p3").css("border-bottom", "3px solid black")
+        $("#p2").css("border-bottom", "none")
+        $("#p1").css("border-bottom", "none")
+        $("#fluid2").css("height", "300px")
+        $("#row2").css("height", "300px")
+        $("#row2").hide();
+        $("#row3").hide();
+        $("#row4").show();
+    })
 
-  
-  });
-  $("#showskin").click(function(){
-      $("#row7").toggle();
-      $("#row6").toggle();
-  
-    
+    $("#p6").click(function () {
+        $("#row6").toggle();
+        $("#row5").toggle();
+
+
     });
-  });
+    $("#showskin").click(function () {
+        $("#row7").toggle();
+        $("#row6").toggle();
 
 
-  const quiz = [
-    {
+    });
+});
+
+
+const quiz = [{
         "question": "How does your skin feel on average?",
         "answers": {
-            "a":"Oily all over",
-            "b":"Oily on certain spots",
-            "c":"Dry and tight",
-            "d":"Not too oily not too dry",
+            "a": "Oily all over",
+            "b": "Oily on certain spots",
+            "c": "Dry and tight",
+            "d": "Not too oily not too dry",
         }
     },
     {
         "question": "What is your main skin concern?",
         "answers": {
-            "a":"Acne and breakouts",
-            "b":"Tight, flaky, dry and dehydrated skin",
-            "c":"Fine lines and wrinkles",
-            "d":"Fading darkspots",
+            "a": "Acne and breakouts",
+            "b": "Tight, flaky, dry and dehydrated skin",
+            "c": "Fine lines and wrinkles",
+            "d": "Fading darkspots",
         }
     },
     {
         "question": "How often do you wash your face?",
         "answers": {
-            "a":"More than twice",
-            "b":"Twice a day",
-            "c":"Once a day",
-            "d":"None of the above",
+            "a": "More than twice",
+            "b": "Twice a day",
+            "c": "Once a day",
+            "d": "None of the above",
         }
     },
     {
         "question": "How many steps would you prefer in your skin care routine?",
         "answers": {
-            "a":"The glow basic- a 3 step routine",
-            "b":"The glow neutral- a 5 step routine",
-            "c":"The glow nebula- 8 step routine",
-            "d":"Any routine",
+            "a": "The glow basic- a 3 step routine",
+            "b": "The glow neutral- a 5 step routine",
+            "c": "The glow nebula- 8 step routine",
+            "d": "Any routine",
         }
     },
     {
         "question": "What is your skin care budget?",
         "answers": {
-            "a":"Affordable price",
-            "b":"I don't care about price",
-            "c":"A little bit expensive",
-            "d":"Negotiable price",
+            "a": "Affordable price",
+            "b": "I don't care about price",
+            "c": "A little bit expensive",
+            "d": "Negotiable price",
         }
     },
 ]
@@ -127,12 +126,21 @@ const getAnswer = () => {
             ans = currentAnswer.id;
         }
     });
-    const corAnswer = {ans1:'a', ans2:'b', ans3:'c', ans4:'d'};
+    const corAnswer = {
+        ans1: 'a',
+        ans2: 'b',
+        ans3: 'c',
+        ans4: 'd'
+    };
     return corAnswer[ans];
 }
 submit.addEventListener('click', () => {
     const checkanswer = getAnswer();
-    console.log({ checkanswer, correct: quiz[count].answers.correct, score});
+    console.log({
+        checkanswer,
+        correct: quiz[count].answers.correct,
+        score
+    });
     if (checkanswer == quiz[count].answers.correct) {
         score = score + 1;
     };
